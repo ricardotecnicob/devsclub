7e36b8a1432d6f0472e65a5a7290f574bf8d4038
Ricardo Alves <ricardotecnicoeletronica@gmail.com>
2019-11-27 08:31:48 -0300

    Alteracoes 27-11

--- src/components/Rank/index.js
+++ src/components/Rank/index.js
@@ -7,55 +7,55 @@ import "./style.css";
 import imagem02 from "../../assets/images/imagem2.jpeg";
 import javascript from "../../assets/images/js-square.svg";
 import python from "../../assets/images/python.png";
-import java from "../../assets/images/java.png";
-import cpp from "../../assets/images/cpp.png";
+import java from "../../assets/images/311945.svg";
+import cpp from "../../assets/images/CPP.png";
 
 const usuarios = [
   {
     imagem: imagem02,
-    nome: "Ricardo Alves",
+    nome: "Ricardo Alve 1",
     linguangem: "javascript",
     linguagemimg: javascript,
     pontuacao: 512
   },
   {
     imagem: imagem02,
-    nome: "Ricardo Alves",
+    nome: "Ricardo Alves 2",
     linguangem: "python",
     linguagemimg: python,
     pontuacao: 500
   },
   {
     imagem: imagem02,
-    nome: "Ricardo Alves",
+    nome: "Ricardo Alves 3",
     linguangem: "javascript",
     linguagemimg: javascript,
     pontuacao: 450
   },
   {
     imagem: imagem02,
-    nome: "Ricardo Alves",
+    nome: "Ricardo Alves 4",
     linguangem: "java",
     linguagemimg: java,
     pontuacao: 400
   },
   {
     imagem: imagem02,
-    nome: "Ricardo Alves",
+    nome: "Ricardo Alves 5",
     linguangem: "c++",
     linguagemimg: cpp,
     pontuacao: 350
   },
   {
     imagem: imagem02,
-    nome: "Ricardo Alves",
+    nome: "Ricardo Alves 6",
     linguangem: "javascript",
     linguagemimg: javascript,
     pontuacao: 300
   },
   {
     imagem: imagem02,
-    nome: "Ricardo Alves",
+    nome: "Ricardo Alves 7",
     linguangem: "python",
     linguagemimg: python,
     pontuacao: 800
@@ -84,23 +84,25 @@ export default class Rank extends Component {
       <div className="areaGeral">
         <div className="menusAcessos">
           <div className="logo">Rank Dev's Club</div>
-          <div className="areasEmpresas">
-            <Link to="/login" className="areaempresas">
-              Acesso Empresas
-            </Link>
+          <div className="menusBuscas">
+              <div className="areasEmpresas">
+                <Link to="/login" className="areaempresas">
+                  Acesso Empresas
+                </Link>
+              </div>
+              <div className="filtro">
+                <select className="selecione" onChange={this.handleChange}>
+                  <option value="todos">todos</option>
+                  <option value="javascript">javascript</option>
+                  <option value="python">python</option>
+                  <option value="ruby">ruby</option>
+                  <option value="java">java</option>
+                  <option value="c++">c++</option>
+                </select>
+              </div>
           </div>
         </div>
 
-        <div className="filtro">
-          <select className="selecione" onChange={this.handleChange}>
-            <option value="todos">todos</option>
-            <option value="javascript">javascript</option>
-            <option value="python">python</option>
-            <option value="ruby">ruby</option>
-            <option value="java">java</option>
-            <option value="c++">c++</option>
-          </select>
-        </div>
 
         {/* Repetido */}
 
